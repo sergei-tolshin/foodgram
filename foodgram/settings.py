0954bad51d93807922951276c58f28a5e2261cd2
Sergey <tolshin.sergei@gmail.com>
2020-11-11 21:11:44 +0500

    Foodgram

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -14,7 +14,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -159,4 +160,4 @@ REST_FRAMEWORK = {
     ],
 }
 
-# SITE_ID = 1
+SITE_ID = 1
